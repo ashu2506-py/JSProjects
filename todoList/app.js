2bfe2c2368c138c36f0e5de89a5dd9275ebb4f28
Ashutosh Singh <ashutoshsingh252006@gmail.com>
2026-09-04 12:28:52 +0530

    Add the event listener on buttons

--- todoList/app.js
+++ todoList/app.js
@@ -3,7 +3,8 @@ document.addEventListener("DOMContentLoaded",()=>{
 const addTask=document.getElementById("addTask")
 const taskList=document.getElementById("todo-list")
 
-let tasks=[]
+let tasks=JSON.parse(localStorage.getItem("tasks")) || []
+tasks.forEach((task)=>renderTask(task))
 addTask.addEventListener("click",()=>{
     const taskText=taskInput.value.trim();
     if (taskText==="") return ;
@@ -13,10 +14,37 @@ addTask.addEventListener("click",()=>{
         completed:false
     }
     tasks.push(newTask);
-    saveTasks()
+    saveTasks();
+    renderTask(newTask)  
     taskInput.value=""
     console.log(tasks);
 })
+
+
+function renderTask(task){
+    console.log(task)
+    const li=document.createElement("li")
+    li.setAttribute("data-id",task.id);
+    if (task.completed) li.classList.add("completed")
+    li.innerHTML=`
+    <span>${task.text}</span>
+    <button>delete</button>`
+
+    li.addEventListener("click",(e)=>{
+        if(e.target.tagName === "BUTTON") return;
+        task.completed=!task.completed;
+        li.classList.toggle("completed")
+        saveTasks()
+    })
+
+    li.querySelector("button").addEventListener("click",(e)=>{
+        e.stopPropagation();
+        tasks=tasks.filter((t)=> t.id!==task.id);
+        li.remove()
+    })
+    taskList.appendChild(li)
+
+}
 
 function saveTasks()
 {
